a74011e8806b3f282831b7e25c4a769a1b368f70
adamnielsen32 <nie15046@byui.edu>
2026-01-31 20:02:25 -0700

    used cors

--- server.js
+++ server.js
@@ -1,35 +1,35 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const { initDb } = require('./db/connect');
 
 const port = process.env.PORT || 3000;
 
-// Swagger 
+// CORS (frontend)
+app.use(cors({
+  origin: [
+    'https://cse341-contacts-frontend.netlify.app'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type']
+}));
+
+app.use(express.json());
+
+// Swagger
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-
-app.use(express.json());
-
-// CORS 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PUT, DELETE, OPTIONS'
-  );
-  next();
-});
-
 // Routes
 app.use('/contacts', require('./routes/contacts'));
+
+// Root (for Render sanity + graders)
+app.get('/', (req, res) => {
+  res.send('Contacts API is running');
+});
 
 // DB + Server
 initDb()
