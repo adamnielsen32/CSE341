8321cb1e66b861547b0876415ba4e7389860fe63
adamnielsen32 <nie15046@byui.edu>
2026-01-31 19:40:31 -0700

    more fixes

--- server.js
+++ server.js
@@ -4,37 +4,39 @@ const express = require('express');
 const app = express();
 const { initDb } = require('./db/connect');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app 
-    .use((req, res, next) => {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Z-Key');
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        next();
-    });
-
+// CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, OPTIONS'
+  );
+  next();
+});
 
 app.use(express.json());
-app.use('/', require('./routes'));
 
+// Routes
+app.use('/contacts', require('./routes/contacts'));
+
+// Swagger
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
-
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
- app.use('/contacts', require('./routes/contacts'));
 
+// DB + Server
 initDb()
   .then(() => {
     app.listen(port, () => {
-        console.log(`Server is running on http://localhost:${port}`);
+      console.log(`Server running on port ${port}`);
     });
-    })
-    .catch((err) => {
-        console.error('Failed to initialize database', err);
-    });
-
-   
-
-
-
+  })
+  .catch((err) => {
+    console.error('Failed to initialize database', err);
+  });
